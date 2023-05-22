7326853f09cbb369d39a457e4095d87933f4673c
Tharamac <tharathorn.wang@gmail.com>
2023-05-23 00:30:59 +0700

    initial yee

---
+++ lib/theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(255, 26, 163, 163);
+const primaryDarkeColor = Color.fromARGB(255, 7, 99, 99);
+const bodyColor = Color.fromARGB(255, 149, 201, 201);
+const secondaryColor = Color.fromARGB(255, 111, 33, 37);
+const secondaryLightColor = Color.fromARGB(255, 208, 49, 59);
+const earColor = Color.fromARGB(255, 239, 191, 193);
